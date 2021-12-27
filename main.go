52987a626076a37e0d095bf4a75b77419b56ac24
Amaury Carrade <amaury@carrade.eu>
2021-12-27 02:51:52 +0100

    Basic bot
    Very basic Discord bot that connects, update the channel name, and leaves.
    Token and channel name are read from the command line (TODO use env).

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"time"
+)
+
+var (
+	Token   string
+	Channel string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Discord bot token")
+	flag.StringVar(&Channel, "c", "", "Discord channel ID")
+}
 
 func main() {
-	fmt.Println("Yo go")
+	loc, err := time.LoadLocation("Europe/Paris")
+	if err != nil {
+		fmt.Println("Unable to load location", err)
+		return
+	}
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error while creating Discord session", err)
+		return
+	}
+
+	channel, err := dg.Channel(Channel)
+	if err != nil {
+		fmt.Println("Error while loading channel", err)
+		return
+	}
+
+	edit := discordgo.ChannelEdit{
+		Name: GenerateChannelName(
+			"covid-{begin}➔{end}",
+			time.Date(2019, time.November, 16, 0, 0, 0, 0, loc),
+			time.Now().In(loc),
+		),
+		// We must specify the position, and cannot use the dg.ChannelEdit shortcut method,
+		// else the channel is moved at the top every time.
+		Position: channel.Position,
+	}
+
+	_, err = dg.ChannelEditComplex(Channel, &edit)
+	if err != nil {
+		fmt.Println("Error while editing channel", err)
+		return
+	}
 }
